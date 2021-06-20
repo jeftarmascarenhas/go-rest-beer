a45b14a8878c3823dddc45a922c80b32f7dd1c48
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-06-19 23:43:55 -0300

    hello world

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	ok, err := say("")
+	if err != nil {
+		panic(err.Error())
+	}
+	switch ok {
+	case true:
+		fmt.Println("Deu certo")
+	default:
+		fmt.Println("deu errado")
+	}
+}
+
+func say(what string) (bool, error) {
+	if what == "" {
+		return false, fmt.Errorf("Empty string")
+	}
+	fmt.Println(what)
+	return true, nil
 }
